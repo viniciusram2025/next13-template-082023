86246930cce5ef78dd5217d4c1305430c3698d6e
Vinicius de Paula <107939188+viniciusram2025@users.noreply.github.com>
2023-08-27 11:12:09 -0300

    Update main.ts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
   stories: [
-    '../components/**/*.stories.@(js|jsx|mjs|ts|tsx)'
+    '../src/components/**/*.stories.@(js|jsx|mjs|ts|tsx)'
   ],
   addons: [
     '@storybook/addon-links',
